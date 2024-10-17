5bed821a1ab1990dae59fb92adf30cb0f67496c2
Pourya-Ka <pouryak.work@gmail.com>
2024-10-17 16:27:12 +0330

    first commit

---
+++ src/main/java/com/pouryat/ta/TechnicalAnalysisApplication.java
@@ -0,0 +1,13 @@
+package com.pouryat.ta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnicalAnalysisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechnicalAnalysisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pouryat/ta/TechnicalAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pouryat.ta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicalAnalysisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
